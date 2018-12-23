f7fd3559f6e3690a8bfb49df893cf49d31e4640d
Юрий <@SEPLOID-PC>
2018-12-24 02:31:54 +0400

    added jdbc beans

--- spring.homework/src/main/java/com/epam/spring/homework/spring/homework/config/AppConfig.java
+++ spring.homework/src/main/java/com/epam/spring/homework/spring/homework/config/AppConfig.java
@@ -1,13 +1,38 @@
 package com.epam.spring.homework.spring.homework.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 @EnableAspectJAutoProxy
+@PropertySource("jdbc.properties")
 @Import({UsersConfig.class, EventsConfig.class})
 public class AppConfig {
+
+    @Value("${jdbc.driverClassName}")
+    String driverClassName;
+
+    @Value("${jdbc.url}")
+    String url;
+
+    @Value("jdbc.username")
+    String username;
+
+    @Value("jdbc.password")
+    String password;
+
+
+    @Bean
+    public JdbcTemplate getJDBCTemplate(){
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName(driverClassName);
+        driverManagerDataSource.setUrl(url);
+        driverManagerDataSource.setUsername(username);
+        driverManagerDataSource.setPassword(password);
+        return new JdbcTemplate(driverManagerDataSource);
+    }
 
 
 }
